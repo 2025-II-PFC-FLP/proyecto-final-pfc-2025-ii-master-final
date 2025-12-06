28df1b014ece5ef473e3695e9f3db97f138d219a
Dev6007 <gabriel.bernal@correounivalle.edu.co>
2025-12-06 18:08:28 -0500

    Prueba de escritorio (Costos)
    Se realizaron 3 pruebas de escritorio para costoRiegoTablon, costoRiegoFinca y costoMovilidad para comprobar su correcto funcionamiento otorgandole vectores con valores hipoteticos, describiendo el proceso y resultado esperado antemano para despues compararse.

--- app/src/test/scala/taller/TestTIRyCostoRiegos.scala
+++ app/src/test/scala/taller/TestTIRyCostoRiegos.scala
@@ -75,6 +75,82 @@ class TestTIRyCostoRiegos extends AnyFunSuite {
 
     assert(resultado == tIResperado)
   }
+
+  test("Ejemplo de costoRiegoTablon hipotetico") {
+
+    val finca: RiegoOptimo.Finca = Vector(
+      (10, 3, 1),   // (tsup, treg, prio)
+      (12, 2, 2),
+      (15, 4, 3)
+    )
+
+    val prog: RiegoOptimo.ProgRiego = Vector(0, 1, 2)
+    // tIR = Vector(0, 3, 5)
+
+    /* Para tablon 0:
+     ts = 10, tr = 3, t = 0, p = 1
+     ts - tr = 7 >= 0  => rama 1
+     costo = 10 - (0+3) = 7*/
+
+    val res = RiegoOptimo.costoRiegoTablon(0, finca, prog)
+    assert(res == 7)
+  }
+
+  test("Ejemplo de costoRiegoFinca (aqui se hace una suma de tablones)") {
+
+    val finca: RiegoOptimo.Finca = Vector(
+      (10, 3, 1),
+      (12, 2, 2),
+      (15, 4, 3)
+    )
+
+    val prog: RiegoOptimo.ProgRiego = Vector(0, 1, 2)
+    // tIR = Vector(0, 3, 5)
+
+    /* Tab.0 = 7  (del test anterior)
+
+     Para tab.1:
+     ts = 12, tr = 2, t = 3
+     ts - tr = 10 >= 3 → rama 1
+     costo = 12 - (3+2) = 7
+
+     Para tab.2:
+     ts = 15, tr = 4, t = 5
+     ts - tr = 11 >= 5 → rama 1
+     costo = 15 - (5+4) = 6
+
+     Total = 20 */
+
+    val res = RiegoOptimo.costoRiegoFinca(finca, prog)
+    assert(res == 20)
+  }
+
+  test("Ejemplo de costoMovilidad con una matriz simple") {
+
+    val finca: RiegoOptimo.Finca = Vector(
+      (10, 3, 1),
+      (12, 2, 2),
+      (15, 4, 3)
+    )
+
+    val prog: RiegoOptimo.ProgRiego = Vector(0, 1, 2)
+
+    val distancias: RiegoOptimo.Distancia = Vector(
+      Vector(0, 5, 9),
+      Vector(5, 0, 4),
+      Vector(9, 4, 0)
+    )
+
+    // pi = Vector(0,1,2)
+    /* se suman:
+       d(0)(1) = 5
+       d(1)(2) = 4
+     total = 9*/
+
+    val res = RiegoOptimo.costoMovilidad(finca, prog, distancias)
+    assert(res == 9)
+  }
+
 }
 
 
